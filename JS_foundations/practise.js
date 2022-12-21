07aa2f438f51d356057b30d2807888caa3d431cb
Dominic Walsh <djcwalsh94@gmail.com>
2022-12-21 09:48:05 +0200

    holiday tings

---
+++ JS_foundations/practise.js
@@ -0,0 +1,21 @@
+let friends = ['dom', 'georgie', 'tim', 'cam', 'matt'];
+
+// console.log(friends[0]);
+friends[0] = "joe"
+// console.log(friends[0]);
+
+
+// let lastItem = friends.pop();
+// console.log(lastItem);
+// friends.push('Menno');
+
+// friends.forEach(friend =>
+//   console.log(friend));
+
+
+// console.log(['lonely'] == 'lonely');
+
+let numbers = [1, 34, 12, 57]
+
+console.log(numbers.sort((a, b) => a > b ? -1 : 1));
+
