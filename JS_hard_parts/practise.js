5d5271a632a359f52429ba88a5d1ec82fb9cc33d
Dominic Walsh <djcwalsh94@gmail.com>
2023-01-11 22:05:54 +0200

    chit

--- JS_hard_parts/practise.js
+++ JS_hard_parts/practise.js
@@ -1,39 +1,59 @@
-function tenSquared(){
-  console.log(10*10)
-}
+// function tenSquared(){
+//   console.log(10*10)
+// }
 
-tenSquared();
-// DRY - Don't repeat yourself.
+// tenSquared();
+// // DRY - Don't repeat yourself.
 
-function squared(number){
-  return number*number
-}
+// function squared(number){
+//   return number*number
+// }
 
-// console.log(squared(12));
+// // console.log(squared(12));
 
-squared(12);
+// squared(12);
 
-// execution context
+// // execution context
 
-function copyArrayAndMultiplyBy2(array) {
-  const output = []
-  for (let i = 0; i < array.length; i++){
-    output.push(array[i] * 2);
+// function copyArrayAndMultiplyBy2(array) {
+//   const output = []
+//   for (let i = 0; i < array.length; i++){
+//     output.push(array[i] * 2);
+//   }
+//   return output;
+// }
+
+// const myArray = [1,2,3];
+// const result = copyArrayAndMultiplyBy2(myArray);
+// console.log(result);
+
+// function copyArrayAndDivideBy2(array) {
+//   const output = [];
+//   for (let i = 0; i < array.length; i++) {
+//     output.push(array[i]/2);
+//   }
+//   return output;
+// }
+
+// const divideResult = copyArrayAndDivideBy2(myArray);
+// console.log(divideResult);
+
+
+// function after(count, func) {
+//   let counter = 0;
+//   return (val) => {
+//     if (++counter >= count) func(val);
+//   }
+// }
+
+const count = 5;
+let counter = 0;
+
+function increment(){
+  if (++counter <= count){
+    console.log(counter);
   }
-  return output;
 }
 
-const myArray = [1,2,3];
-const result = copyArrayAndMultiplyBy2(myArray);
-console.log(result);
-
-function copyArrayAndDivideBy2(array) {
-  const output = [];
-  for (let i = 0; i < array.length; i++) {
-    output.push(array[i]/2);
-  }
-  return output;
-}
-
-const divideResult = copyArrayAndDivideBy2(myArray);
-console.log(divideResult);
+increment();
+increment();
