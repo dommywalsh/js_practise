eeaa690b5bb967c64294d5cf6d604bd1d98b56cc
Dominic Walsh <djcwalsh94@gmail.com>
2023-01-24 15:50:39 +0200

    learning the basics of NODE

---
+++ intro_to_node/package/index.mjs
@@ -0,0 +1,8 @@
+import _ from 'lodash'
+
+console.log(_)
+
+
+// if you 't put './' when importing i.e. import _ from './index.mjs' it looks in your local environment.
+// when you don't put that it is either an internal module like fs or it is something you have installed from NPM or an equivalent
+// npm uninstall lodash ... 
